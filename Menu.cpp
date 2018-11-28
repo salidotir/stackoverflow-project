e83c00e0f6b063cb932d9ab00e53c41577c07957
Sara Limooee <35997721+salidotir@users.noreply.github.com>
2018-11-28 19:55:00 +0330

    Add files via upload

--- Menu.cpp
+++ Menu.cpp
@@ -1,234 +1,348 @@
 #include "Menu.h"
+#include <fstream>
+#include <sstream>
+MenuState menuState = MenuState::START;
+string last_message;
+User * loggedInUser = nullptr;
+
+vector<string> split (string s, string delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    string token;
+    vector<string> res;
+
+    while ((pos_end = s.find (delimiter, pos_start)) != string::npos) {
+        token = s.substr (pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back (token);
+    }
+
+    res.push_back (s.substr (pos_start));
+    return res;
+}
+struct search1{
+    string us;
+    string n;
+};
 
 void Menu::ShowGuestMenu()
-	{
-		cout << "*****************Menu*****************" << endl << endl;
-		for (int i = 0; i < size1; i++)
-		{
-			cout << _guestMenuItems[i] << endl;
-		}
-	}
+{
+    cout << "*****************Menu*****************" << endl << endl;
+    for (int i = 0; i < size1; i++)
+    {
+        cout << _guestMenuItems[i] << endl;
+    }
+}
 
-	void Menu::Login()
-	{
-		try {
-			string username, password;
-			cout << "Enter Username: ";
-			cin >> username;
-			cout << "Enter Password: ";
-			cin >> password;
-			loggedInUser = &User::login(username, password);
-			menuState = MenuState::LOGGED_IN;
-		}
-		catch (WrongUsernameOrPasswordException &e) {
-			last_message = e.what();
-		}
-	}
+void Menu::Login()
+{
+    try {
+        string username, password;
+        cout << "Enter Username: ";
+        cin >> username;
+        cout << "Enter Password: ";
+        cin >> password;
+        loggedInUser = &User::login(username, password);
+        menuState = MenuState::LOGGED_IN;
+    }
+    catch (WrongUsernameOrPasswordException &e) {
+        last_message = e.what();
+    }
+}
 
-	void Menu::SignUp()
-	{
-		try {
-			string username, password, email;
-			cout << "Enter Username: ";
-			cin >> username;
-			cout << "Enter Password: ";
-			cin >> password;
-			cout << "Enter email: ";
-			cin >> email;
-			loggedInUser = &User::signup(username, password, email);
-			menuState = MenuState::LOGGED_IN;
-			last_message = "User signed up!\n";
-		}
-		catch (UserAlreadyExistsException &e) {
-			last_message = e.what();
-		}
-	}
+void Menu::SignUp()
+{
+    try {
+        string username, password, email;
+        cout << "Enter Username: ";
+        cin >> username;
+        cout << "Enter Password: ";
+        cin >> password;
+        cout << "Enter email: ";
+        cin >> email;
+        loggedInUser = &User::signup(username, password, email);
+        menuState = MenuState::LOGGED_IN;
+        last_message = "User signed up!\n";
+    }
+    catch (UsernameAlreadyExistsException &e) {
+        last_message = e.what();
+    }
+}
 
-	void Menu::ShowAllUsers()
-	{
-		DBAccess dbAccess;
-		string readUserName;
-		string readPassWord;
-		string readEmail;
-		Logger lg;
-		int num = dbAccess.GetNumberOfUsers();
+void Menu::ShowAllUsers()
+{
+    int flag = 0;
+    string us;
+    fstream input_file;
+    input_file.open("./Users/AllUsers.txt", ios::in | ios::binary);
+    while(!input_file.eof())
+    {
+        if(!input_file){
+            cout << "Error in opening file.";
+            //return 0;
+        }
+        if(flag == 0){
+            getline(input_file, us);
+            getline(input_file, us);
+            flag = 1;
+        }
+        else if(flag == 1){
+            getline(input_file, us);
+            getline(input_file, us);
+            getline(input_file, us);
+        }
+        cout << us << endl;
+    }
+    input_file.close();
+}
 
-		ifstream file;
-		file.open("Users\\users.bin", ios::in | ios::binary);
-		file.seekg(0, file.beg);
-		for (int i = 0; i < num; i++)
-		{
-			if (!file.eof()) {
-				file.read((char*)&readUserName, sizeof(string));
-				file.read((char*)&readPassWord, sizeof(string));
-				file.read((char*)&readEmail, sizeof(string));
+void Menu::SearchForSpecialTag(){
+    system("clear");
+    vector<search1> find;
+    cout<<"Please enter tag:";
+    string tagname;
+    cin>>tagname;
+    string s="./Questions/AllQuestions.txt";
+    fstream file;
+    file.open(s,ios::binary | ios::in);
+    string question1;
 
-				cout << "******************************" << endl;
-				cout << "username : " << readUserName << endl;
-				cout << "email : " << readEmail << endl;
-				cout << "******************************" << endl;
-			}
-		}
-		file.close();
-	}
+    while (!file.eof()){
+        string question,username,tags,n,type;
+        getline(file,question);
+        getline(file,username);
+        getline(file,tags);
+        getline(file,n);
+        getline(file,type);
+        vector<string>v;
+        v=split(tags,"\t") ;
+        for (auto i = v.begin(); i <v.end() ; i++) {
 
-	void Menu::SearchForSpecialUser()
-	{
-		cout << "Which search would you like to choose?" << endl;
-		cout << "1. Based on username" << endl;
-		cout << "2. Based on email" << endl;
+            if (tagname.compare(*i)==0)   {
+                search1 a;
+                a.us=username;
+                a.n=n;
+                find.push_back(a);
+                break;
+            }
+        }}
+    file.close();
+    for (auto j = find.begin(); j <find.end() ; j++) {
+        string s1="./Users/"+(*j).us+"/contents/Q/"+(*j).n+".txt";
+        fstream file1;
+        file1.open(s1,ios::binary | ios::in);
+        string s2;
+        while (getline(file1,s2)){
 
-		int choice;
-		cin >> choice;
-		switch (choice)
-		{
-		case 1:
-			break;
-		case 2:
-			break;
-		default:
-			cout << "Wrong choice!" << endl;
-			break;
-		}
-	}
+            cout<<s2<<endl;
 
-	int Menu::Exit()					// function to go out of program.
-	{
-		return 0;
-	}
+        }
+        file1.close();
+        cout<<"1:next\n2:previous\n3.Answer this question\n4:stop\n";
+        char input;
+        cin>>input;
+        switch (input){
+            case '1':
+                continue;
+            case '2':
+                j--;
+                j--;
+                continue;
+            case '3':
+                //(*loggedInUser).add_answer();
+                last_message="Answer added.\n";
+            case '4':
+                return;
+        }
+        // string question1,tags,n,type;
 
-	void Menu::ShowUserMenu()
-	{
-		cout << "*****************Menu*****************" << endl << endl;
-		for (int i = 0; i < size2; i++)
-		{
-			cout << _userMenuItems[i] << endl;
-		}
-	}
+        //  getline(file,question);
 
-	void Menu::ShowRecentQuestions()
-	{
+    }
+}
 
-	}
 
-	void Menu::AskANewQuestion()
-	{
 
-	}
+void Menu::ShowUserMenu()
+{
+    cout << "*****************Menu*****************" << endl << endl;
+    for (int i = 0; i < size2; i++)
+    {
+        cout << _userMenuItems[i] << endl;
+    }
+}
 
-	void Menu::AnswerAQuestion()
-	{
+void Menu::ShowRecentQuestions()
+{
 
-	}
+}
 
-	void Menu::ShowAllQuestions()
-	{
+void Menu::AskANewQuestion()
+{
+    (*loggedInUser).add_question();
+    last_message="Question added.\n";
+}
 
-	}
+void Menu::EditQuestion() {
+    try {
+        (*loggedInUser).edit_question();
+        last_message="Question changed.\n";
+    }catch (QuestionNotFound &e){
+        last_message = e.what();
+    }
 
-	void Menu::Logout()
-	{
-		menuState = MenuState::START;
-	}
+}
+void Menu::ShowAllQuestions()
+{int ten=1;
+    string s="./Questions/AllQuestions.txt";
+    fstream file;
+    file.open(s,ios::binary|ios::in);
+    int counterque=1;
+    while(!file.eof()) {
+        char input;
+        do {
+            while (ten != 10 && !file.eof()) {
+                if (!file) {
+                    cout << "Error in opening file..";
+                    //return 0;
+                }
+                string tmp;
+                getline(file, tmp);
+                cout<<counterque<<" : ";
+                counterque++;
+                cout << tmp;
+                cout << "\n";
+                for (int i = 0; i < 4; ++i) {
+                    getline(file, tmp);
+                }
+                ten++;
+            }
+            char qnum;
+            cout<<"Do you want to see one of the above?(y/n)"<<endl;
+            cin>>qnum;
+            if(qnum=='y'){
+                cout<<"Enter number of question: ";
+                int number;
+                cin>>number;
+                cout<<"call answer function later.\n";
+                return;
+            }
+            if(qnum=='n'){
+            cout << "Do you want to continue?(y/n)" << endl;
+            cin >> input;
+            ten=0;
+            }
+        }while(input=='y' && !file.eof());
+        return;
+    }
+    file.close();
+    return;
+}
 
-	void Menu::DeleteAccount()
-	{
+void Menu::Logout()
+{
+    menuState = MenuState::START;
+}
 
-	}
+void Menu::DeleteAccount()
+{
+    (*loggedInUser).deletAccount();
+    last_message = "Account deleted.\n";
+}
 
-	void Menu::RunMenu()
-	{
-		static int temp = 0;
-		User::init("SECRET_KEY");
 
-		char choice;
-		while (menuState != MenuState::END) {
-			system(CLEAR);
-			if (!last_message.empty())
-				cout << last_message << endl;
-			last_message = "";
-			if (temp++ != 0)
-			{
-				system("pause");
-				system(CLEAR);
-			}
-			switch (menuState) {
-			case MenuState::START: {
+void Menu::RunMenu()
+{create();
+    static int temp = 0;
+    User::init("SECRET_KEY");
+    char choice;
+    while (menuState != MenuState::END) {
+        system(CLEAR);
+        if (!last_message.empty())
+            cout << last_message << endl;
+        last_message = "";
+        if (temp++ != 0)
+        {
+            system("read");
+            system(CLEAR);
+        }
+        switch (menuState) {
+            case MenuState::START: {
 
-				ShowGuestMenu();
-				cout << "Please choose what to do : ";
-				cin >> choice;
+                ShowGuestMenu();
+                cout << "Please choose what to do : ";
+                cin >> choice;
 
-				switch (choice) {
-				case '1': // sign up
-					SignUp();
-					break;
-				case '2': // login
-					Login();
-					break;
-				case 3:
-					ShowRecentQuestions();
-					break;
-				case 4:
-					ShowAllQuestions();
-					break;
-				case 5:
-					AskANewQuestion();
-					break;
-				case 6:
-					ShowAllUsers();
-					break;
-				case 7:
-					Exit();
-					break;
-				default: // unknown input
-					last_message = "unknown input\n";
-					break;
-				}
-				break;
-			}
-			case MenuState::LOGGED_IN: {
-				ShowUserMenu();
-				cout << "Please choose what to do : ";
-				cin >> choice;
+                switch (choice) {
+                    case '1': // sign up
+                        SignUp();
+                        break;
+                    case '2': // login
+                        Login();
+                        break;
+                    case '3':
+                        ShowRecentQuestions();
+                        break;
+                    case '4':
+                        ShowAllQuestions();
+                        break;
+                    case '5':
+                        ShowAllUsers();
+                        break;
+                    case '6':
 
-				switch (choice) {
-				case '1': // logout
-					Logout();
-					break;
+                        return ;
+                        break;
+                    default: // unknown input
+                        last_message = "unknown input\n";
+                        break;
+                }
+            }
+                continue;
+            case MenuState::LOGGED_IN: {
+                ShowUserMenu();
+                cout << "Please choose what to do : ";
+                cin >> choice;
 
-				case '2': // delete account
-					DeleteAccount();
-					break;
-				case 3:
-					ShowRecentQuestions();
-					break;
-				case 4:
-					ShowAllQuestions();
-					break;
-				case 5:
-					AskANewQuestion();
-					break;
-				case 6:
-					AnswerAQuestion();
-					break;
-				case 7:
-					ShowAllUsers();
-					break;
-				case 8:
-					SearchForSpecialUser();
-					break;
-				case 9:
-					Exit();
-					break;
-				default: // unknown input
-					last_message = "unknown input\n";
-					break;
-				}
-				break;
-			}
-			}
-			system(CLEAR);
-			cout << "GOODBYE" << endl;
-		}
-	}
+                switch (choice) {
+                    case '1': // logout
+
+                        Logout();
+                        break;
+
+                    case '2': // delete account
+                        DeleteAccount();
+                        break;
+                    case '3':
+                        ShowRecentQuestions();
+                        break;
+                    case '4':
+                        ShowAllQuestions();
+                        break;
+                    case '5':
+                        AskANewQuestion();
+                        break;
+                    case '6':
+                        ShowAllUsers();
+                        break;
+                    case '7':
+                        SearchForSpecialTag();
+                        break;
+                    case '8':
+                        EditQuestion();
+                        break;
+                    case '0':
+                        return ;
+                        break;
+                    default: // unknown input
+                        last_message = "unknown input\n";
+                        break;
+                }
+                //continue;
+            }
+                continue;
+        }
+        system(CLEAR);
+        cout << "GOODBYE" << endl;
+    }
+}
+
--- Menu.h
+++ Menu.h
@@ -1,13 +1,17 @@
+//
+// Created by negin on 11/26/18.
+//
+
+#ifndef STACK_MENU_H
+#define STACK_MENU_H
 #pragma once
-#ifndef STACKOVERFLOW_IN_CPP1_MENU_H
-#define STACKOVERFLOW_IN_CPP1_MENU_H
 
 #include <iostream>
 #include <string>
 #include "AbstractUser.h"
 #include "Exceptions.h"
 #include "User.h"
-
+#include "database.h"
 #ifdef _WIN32
 #define CLEAR "cls"
 #else //In any other OS
@@ -17,68 +21,65 @@
 using namespace std;
 
 enum MenuState {
-	START,
-	LOGGED_IN,
-	END
+    START,
+    LOGGED_IN,
+    END
 };
 
-User * loggedInUser = nullptr;
-MenuState menuState = MenuState::START;
-string last_message;
+
 
 class Menu {
 public:
-	string _guestMenuItems[10] = {
-		"1. Sign up",
-		"2. Login",
-		"3. Show recent questions",
-		"4. Show all questions",
-		"5. Ask a new question",
-		"6. show all users",
-		"7. Exit"
-	};
-	int size1 = 7;
+    friend User;
+    string _guestMenuItems[10] = {
+            "1. Sign up",
+            "2. Login",
+            "3. Show recent questions",
+            "4. Show all questions",
+            "5. Show all users",
+            "6. Exit"
+    };
+    int size1 = 6;
 
-	string _userMenuItems[10] = {
-		"1. logout",
-		"2. delete account",
-		"3. Show recent questions",
-		"4. Show all questions",
-		"5. Ask a new question",
-		"6. answer a question",
-		"7. See all users",
-		"8. Search for a special user",
-		"9. Exit"
-	};
-	int size2 = 9;
+    string _userMenuItems[10] = {
+            "1. logout",
+            "2. delete account",
+            "3. Show recent questions",
+            "4. Show all questions",
+            "5. Ask a new question",
+            "6. See all users",
+            "7. Search for a special tag",
+            "8. Edit question",
+            "0. Exit"
+    };
+    int size2 = 10;
 
-	void ShowGuestMenu();
+    void ShowGuestMenu();
 
-	void Login();
+    void Login();
 
-	void SignUp();
+    void SignUp();
 
-	void ShowAllUsers();
+    void ShowAllUsers();
+    void EditQuestion();
+    void SearchForSpecialTag();
 
-	void SearchForSpecialUser();
+    // function to go out of program.
 
-	int Exit();					// function to go out of program.
+    void ShowUserMenu();
 
-	void ShowUserMenu();
+    void ShowRecentQuestions();
 
-	void ShowRecentQuestions();
+    void AskANewQuestion();
 
-	void AskANewQuestion();
+    void ShowAllQuestions();
 
-	void AnswerAQuestion();
+    void Logout();
 
-	void ShowAllQuestions();
+    void DeleteAccount();
 
-	void Logout();
-
-	void DeleteAccount();
-
-	void RunMenu();
+    void RunMenu();
 };
 
-#endif //STACKOVERFLOW_IN_CPP1_MENU_H
+
+#endif //STACK_MENU_H
